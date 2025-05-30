d7495ee8d647926a9f3835107f58b13778640037
Ivan <vanya0150396@gmail.com>
2025-05-30 19:26:42 +0300

    start create models

--- WebApp/src/main/java/efes/webApp/models/Device.java
+++ WebApp/src/main/java/efes/webApp/models/Device.java
@@ -1,4 +1,6 @@
 package efes.webApp.models;
 
 public class Device {
+    private String name;
+    private int sapNumber;
 }
---
+++ WebApp/src/main/java/efes/webApp/models/Subdivision.java
@@ -0,0 +1,12 @@
+package efes.webApp.models;
+
+public enum Subdivision {
+    VKS,
+    BOILERROOM,
+    CO2,
+    BEERPRODUCTION,
+    PACKINGPRODUCTION,
+
+
+
+}
