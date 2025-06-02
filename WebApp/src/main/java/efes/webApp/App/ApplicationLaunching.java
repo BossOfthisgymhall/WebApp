b98bdc8227926e6533ec6a9e16c0dd9a1d7e3d1a
Ivan <vanya0150396@gmail.com>
2025-06-02 05:44:37 +0300

    Spring error

--- WebApp/src/main/java/efes/webApp/App/ApplicationLaunching.java
+++ WebApp/src/main/java/efes/webApp/App/ApplicationLaunching.java
@@ -2,10 +2,12 @@ package efes.webApp.App;
 
 import efes.webApp.fileReader.FileReader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationLaunching {
+    @Autowired
     private String filePath;
     FileReader fileReader;
     @Autowired
